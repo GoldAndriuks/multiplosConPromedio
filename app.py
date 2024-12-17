54bf3069423e6f1750466603b7500881c3fc785f
GoldAndriuks <eh2302is387@ueh.edu.mx>
2024-12-17 12:48:17 -0600

    correr flask

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    numeros = ["12000", "14000", "15940"]
+    primos = [
+        "el ultimo numero primo de 1200 es 11987",
+        "el ultimo numero primo de 14000 es 13999",
+        "el ultimo numero primo de 15940 es 15937"
+    ]
+    longitud = len(numeros)  # Calculo la longitud una sola vez
+    return render_template('index.html', numeros=numeros, primos=primos, longitud=longitud)
+
+
